00a789573a82178a1b20221cd318a6829aadca16
HughParsonage <hugh.parsonage@gmail.com>
2018-02-25 19:35:59 +1100

    Fix travis syntax; complete test

--- tests/test_grattex.R
+++ tests/test_grattex.R
@@ -1,7 +1,14 @@
 if (nzchar(Sys.which("pdftotext")) && file.exists("Report.pdf")) {
   system("pdftotext -layout Report.pdf")
+  library(data.table)
   
-  Report.pdf <- readLines("Report.txt", warn = FALSE)
-  cat(head(Report.pdf), sep = "\n")
-  file.remove("Report.txt")
+  Report.pdf <-
+    data.table(pdf_lines = readLines("Report.txt", warn = FALSE, encoding = "UTF-8"))
+  
+  
+  # Issue 102
+  Ibid_lines <- Report.pdf[grepl("Ibid. (P", pdf_lines, fixed = TRUE)]
+  if (nrow(Ibid_lines)) {
+    stop("Ibid contains a page reference with uppercase P.")
+  }
 }
